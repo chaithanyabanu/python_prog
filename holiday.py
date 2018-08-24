76c478176691b9e12f5ec5a72af2483e0c3c2ce8
chaithanyabanu <chaithanyabanu@gmail.com>
2018-08-24 10:40:58 +0530

    This used to check the given date is weekend

---
+++ holiday.py
@@ -0,0 +1,15 @@
+from datetime import date
+from datetime import datetime
+def holiday(day):
+	if day%6==0 or day%5==0:
+		print("hii this is weekend you can enjoy holiday")
+
+	else:
+		print("come to the office immediately")
+
+date_to_check_holiday=input("enter the date required to check")
+
+today=date(year,month,day)
+day_count=today.weekday()
+print(day_count)
+holiday(day_count)
